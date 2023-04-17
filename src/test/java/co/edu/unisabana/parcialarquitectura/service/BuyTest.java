3c4b08ff6d28dadeba22f66ba2024cbeeab86398
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-04-16 22:23:00 -0500

    agrega prueba de integración continua

--- src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
+++ src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
@@ -1,39 +1,30 @@
 package co.edu.unisabana.parcialarquitectura.service;
 
 import co.edu.unisabana.parcialarquitectura.repository.Database;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+@ExtendWith(MockitoExtension.class)
 class BuyTest {
-
-    private final Buy service = new Buy(new Database());
-    @ExtendWith(MockitoExtension.class)
     @InjectMocks
     Buy buy;
+    @Mock
+    Database database;
 
     @Test
-    public void test() {
-        service.makePurchase(2, 3, "Books");
+    void Given_vendedorYcompradorSonIguales_When_invoke_makePurchase_Then_IllegalSaleException()
+    {
+        int buyCode=1, vendorCode=1;
+        String item="carro";
+        Assertions.assertThrows(IllegalSaleException.class, () -> {
+            buy.makePurchase(buyCode,vendorCode,item);
+        });
     }
 
-    @Test
-    public void Given_codes_When_buyer_code_and_vendor_code_equals_Then_verify_throw_Illegal_exception() {
-        assertThrows(IllegalSaleException.class, ()-> service.makePurchase(3, 3, "Books"));
-
-    }
-    @Test
-    public void Given_vendor_and_different_buyer_When_purchase_Then_sold() {
-        service.makePurchase(2, 3, "Books");
-        assertEquals("Product sold", service.makePurchase(2, 3, "Books"));
-    }
-    @Test
-    public void Given_vendor_and_different_buyer_with_null_item_When_purchase_Then_sale_couldnt_be_possible() {
-        assertEquals("The sale was not possible", service.makePurchase(2, 3, null));
-    }
 
 }
